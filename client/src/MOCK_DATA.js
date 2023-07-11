e9f610bfbda2b64f201c8eb125f2fd763c01a940
Georgi <ggeorgeuk@gmail.com>
2023-07-11 08:51:25 +0100

    update

--- client/src/MOCK_DATA.js
+++ client/src/MOCK_DATA.js
@@ -2,136 +2,135 @@ export const Data = [
   {
     "question": "Internet",
     "answer": [" Global system of interconnected computer networks that uses the Internet protocol suite (TCP/IP) to communicate between networks and devices."],
-    "path": ""
+  
   },
   {
     "question": "WWW",
     "answer": [" Information system enabling information to be shared over the Internet."],
-    "path": ""
   },
   {
     "question": "JavaScript",
     "answer": [" Programming language that is one of the core technologies of the World Wide Web."],
-    "path": ""
+
   },
   {
     "question": "Values",
     "answer": [" Values represent task we want to accomplish.", "Values have an behavior associated with them."],
-    "path": require("./images/values.png"),  
+
   },
   {
     "question": "Operations",
     "answer": [" Operation is what we do with our values."],
-    "path": require("./images/operations.png"),
+  
   },
   {
     "question": "Types",
     "answer": [" Type is referring with kind of the value."],
-    "path": require("./images/types.png")
+
   },
   {
     "question": "Variables",
     "answer": [" Variable is a representation in some place in memory where we store data."],
-    "path": require("./images/types.png")
+
   },
   {
     "question": "Expressions",
     "answer": [" Expression is part of entire thing."],
-    "path": require("./images/expressions.png")
+
   },
   {
     "question": "Statements",
     "answer": [" Statement is an entire thing finish with semicolon."],
-    "path": require("./images/statements.png")
+  
   },
   {
     "question": "Conditions",
     "answer": [" Conditions refer to decision making. if statement is the primary way to make a decision."],
-    "path": require("./images/conditions.png")
+  
   },
   {
     "question": "Loops",
     "answer": [" When have set of operation to do multiple times comes Loops"],
-    "path": require("./images/loops.png")
+   
   },
   {
     "question": "Functions",
     "answer": [" Function is a collection of statements which we want to do multiple time in multiple places."],
-    "path": require("./images/functions.png")
+  
   },
   {
     "question": "Primitive data type",
     "answer": [" This is the predefined data type that is provided by JavaScript for different usages."," These are also known as the in-built data types."],
-    "path": require("./images/primitives.png")
+
   },
   {
     "question": "NaN",
     "answer": [ "NaN represents the result of an invalid math operation.", " NaN is invalid number." ],
-    "path": require("./images/nan.png")
+   
   },
   {
     "question": "new",
     "answer": [" Keyword 'new' create object wrapper around build in fundamental objects."],
-    "path": require("./images/new.png")
+  
   },
   {
     "question": "Coersion",
     "answer": [" The way to convert from one type to another called Coersion", " JavaScript do it dynamically."],
-    "path": require("./images/coersion.png")
+
   },
   {
     "question": "Booleans",
     "answer": [" Biger concept called falsy and truthy."],
-    "path": require("./images/booleans.png")
+   
   },
   {
     "question": "Equality",
     "answer": [" Cheking equality with '==' and '==='", "When types same, both opearors do same job."],
-    "path": require("./images/equality.png")
+  
   },
   {
     "question": "Scope",
     "answer": [" Rules for JavaScript engine when looks for identifiers."],
-    "path": require("./images/scope.png")
+
   },
   {
     "question": "Emptiness",
     "answer": [" State of Undeclared and Undefined"],
-    "path": require("./images/emptiness.png")
+
   },
   {
     "question": "Function Expressions",
     "answer": [" Function is a first class citizen in a language.", " Function Expression is a function that can be passed around."],
-    "path": require("./images/funcexp.png")
+ 
   },
   {
     "question": "IIFE",
     "answer": [" IIFE (Immediately Invoked Function Expression) make new block of scope."],
-    "path": require("./images/iife.png")
+
   },
   {
     "question": "Block Scoping",
     "answer": [" Way to organise set of variables with own scope instead of IIFE."],
-    "path": require("./images/let.png")
+
   },
   {
     "question": "Closure",
     "answer": [" When a function 'remember' variables outside of it, even if we pass that function around."],
-    "path": require("./images/closure.png")
+ 
   },
   {
     "question": "this",
     "answer": [" this: ..."],
-    "path": ""
+  
   },
-  // {
-  //   "question": "Prototypes",
-  //   "answer": [" Prototypes: ..."],
-  //   "path": ""
-  // },
+  {
+    "question": "Prototype system",
+    "answer": [" Prototype is an object which all instances delegate to."],
+
+  },
   {
     "question": "Class",
-    "answer": [" Class: ..."],
-    "path": ""
+    "answer": ["Layer on top of prototype system."],
+    // "path": require("./images/classes.png")
   }
 ]
